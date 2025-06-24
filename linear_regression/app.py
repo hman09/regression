f1e2a5818989a4037ed5fe3779cf9f1b0ba4abe9
hman09 <14950@coderacademy.edu.au>
2025-06-24 13:44:03 +1000

    model started

--- linear_regression/app.py
+++ linear_regression/app.py
@@ -1,6 +1,8 @@
 import numpy as  np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
+# ------- Table and Graph Setup -------
 
 def make_data(n_sample=100):
     rnd = np.random.RandomState(42)
@@ -10,7 +12,24 @@ def make_data(n_sample=100):
     y *= 5
     return x.reshape(-1,1), y.reshape(-1,1)
 
-x, y = make_data()
+x,y = make_data()
 
 print(y.shape)
 print(x.shape)
+
+# plt.scatter(x,y)
+# plt.title("Scatter Plot")
+# plt.xlabel("Advertising Expeditire (million dollar)")
+# plt.ylabel("Sales Revenue (million dollar)")
+# plt.show()
+
+# ------- Define Cost Function -------
+
+#  J(θ0,θ1) = 1/2m ∑mi=1 (hθ(x^(i))−y^(i)^)2
+
+# m is number of examples
+m = len(y)
+print(m)
+
+
+
